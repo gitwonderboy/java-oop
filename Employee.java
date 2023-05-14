9d66bafe0460652202729464944419db3f96f6d3
Wonderboy Mthiyane <codelad1@gmail.com>
2023-05-14 14:49:10 +0200

    Added: Main (main method) and Employee (attributes and behavior)
    Signed-off-by: Wonderboy Mthiyane <codelad1@gmail.com>

--- Employee.java
+++ Employee.java
@@ -1,20 +1,54 @@
 public class Employee {
 	// Class Scope
 	private String name;
-	private int employee_id;
+	private int employeeId;
 	private double salary;
 	private String[] complaints;
 
-	public Employee(String name, int employee_id, double salary) {
+	public Employee(String name, int employeeId, double salary) {
 		// Method Scope
 		// (this) The term key-word refers to class attributes.
 		this.name = name;
-		this.employee_id = employee_id;
+		this.employeeId = employeeId;
 		this.salary = salary;
 		this.complaints = new String[10];
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public double getSalary() {
+		return salary;
 	}
 
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
+
+	public String[] getComplaints() {
+		return complaints;
+	}
+
+	public void setComplaints(String complaint) {
+		for (int i = 0; i <= complaints.length; i++) {
+			if (complaints[i] == null) {
+				complaints[i] = complaint;
+				break;
+			}
+		}
+	}
+
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
 		Employee employee = new Employee("Wonderboy", 1, 20000);
-		System.out.println(employee);
+		employee.setComplaints("Take long to respond");
+		System.out.println(employee.getName());
+		System.out.println(employee.getSalary());
+		System.out.println(employee.getComplaints()[0]);
 	}
 }
